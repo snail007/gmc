fe9f723e2a3f60624c7d30a2bb8223c1983cc08a
arraykeys <arraykeys@gmail.com>
2020-09-08 17:33:49 +0800

    add goroutine pool

---
+++ sync/pool/gpool.go
@@ -0,0 +1,122 @@
+package gpool
+
+import (
+	"context"
+	"log"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type GPool struct {
+	tasks       []func()
+	lock        sync.Mutex
+	ctx         context.Context
+	cancel      context.CancelFunc
+	runningtCnt *int32
+	logger      *log.Logger
+	workerCnt   int
+}
+
+func New(workerCount int) (p *GPool) {
+	cnt := int32(0)
+	ctx0, cancel0 := context.WithCancel(context.Background())
+	p = &GPool{
+		tasks:       []func(){},
+		lock:        sync.Mutex{},
+		ctx:         ctx0,
+		cancel:      cancel0,
+		runningtCnt: &cnt,
+		workerCnt:   workerCount,
+		logger:      log.New(os.Stdout, "", log.LstdFlags),
+	}
+	p.init()
+	return
+}
+
+func (s *GPool) init() {
+	go func() {
+		defer func() {
+			if e := recover(); e != nil {
+				s.log("GPool stopped unexceptedly, err: %s", e)
+			}
+		}()
+		for i := 0; i < s.workerCnt; i++ {
+			go func(i int) {
+				defer func() {
+					if e := recover(); e != nil {
+						s.log("GPool: a worker stopped unexceptedly, err: %s", e)
+					}
+				}()
+				// s.log("GPool: worker[%d] started ...", i)
+				ctx, cancle := context.WithCancel(s.ctx)
+				defer cancle()
+				for {
+					select {
+					case <-ctx.Done():
+						// s.log("GPool: worker[%d] stopped.", i)
+						return
+					default:
+						if fn := s.pop(); fn != nil {
+							atomic.AddInt32(s.runningtCnt, 1)
+							s.run(fn)
+							atomic.AddInt32(s.runningtCnt, -1)
+							fn = nil
+						} else {
+							time.Sleep(time.Second * 3)
+						}
+					}
+				}
+			}(i)
+		}
+	}()
+}
+
+func (s *GPool) run(fn func()) {
+	defer func() {
+		if e := recover(); e != nil {
+			s.log("GPool: a task stopped unexceptedly, err: %s", e)
+		}
+	}()
+	fn()
+}
+func (s *GPool) Submit(task func()) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.tasks = append(s.tasks, task)
+}
+
+func (s *GPool) pop() (fn func()) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	l := len(s.tasks)
+	if l > 0 {
+		fn = s.tasks[0]
+		s.tasks[0] = nil
+		if l == 1 {
+			s.tasks = []func(){}
+		} else {
+			s.tasks = s.tasks[1:]
+		}
+	}
+	return
+}
+func (s *GPool) Stop() {
+	s.cancel()
+}
+func (s *GPool) Running() (cnt int32) {
+	return atomic.LoadInt32(s.runningtCnt)
+}
+func (s *GPool) Awaiting() (cnt int32) {
+	return int32(len(s.tasks))
+}
+func (s *GPool) log(fmt string, v ...interface{}) {
+	if s.logger == nil {
+		return
+	}
+	s.logger.Printf(fmt, v...)
+}
+func (s *GPool) SetLogger(l *log.Logger) {
+	s.logger = l
+}

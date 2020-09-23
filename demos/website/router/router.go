c22036714d0f6496c7cb97f5d24c94ae06af436d
snail007 <arraykeys@gmail.com>
2020-09-23 14:42:45 +0800

    demo comment

--- demos/website/router/router.go
+++ demos/website/router/router.go
@@ -11,10 +11,20 @@ import (
 )
 
 func InitRouter(s *gmchttpserver.HTTPServer) {
+	// sets pre routing handler, it be called with any request.
 	s.BeforeRouting(filiterAll)
+
+	// sets post routing handler, it be called only when url's path be found in router.
 	s.RoutingFiliter(filiter)
+
+	// acquire router object
 	r := s.Router()
+
+	// bind a controller, /demo is path of controller, after this you can visit http://127.0.0.1:7080/demo/hello
+	// "hello" is full lower case name of controller method.
 	r.Controller("/demo", new(controller.Demo))
+
+	// indicates router initialized
 	s.Logger().Printf("router inited.")
 }
 func filiterAll(w http.ResponseWriter, r *http.Request, server *gmchttpserver.HTTPServer) bool {
@@ -23,6 +33,8 @@ func filiterAll(w http.ResponseWriter, r *http.Request, server *gmchttpserver.HT
 }
 func filiter(w http.ResponseWriter, r *http.Request, ps gmcrouter.Params, server *gmchttpserver.HTTPServer) bool {
 	path := strings.TrimRight(r.URL.Path, "/\\")
+
+	// we want to prevent user to access method `controller.Demo.Protected`
 	if strings.HasSuffix(path, "protected") {
 		w.Write([]byte("404"))
 		return false

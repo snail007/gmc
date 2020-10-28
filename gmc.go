5065bc3482fc4fa5a3558db7b9740c09b3515460
arraykeys <arraykeys@gmail.com>
2020-10-28 10:53:04 +0800

    cast helper add more comments

--- gmc.go
+++ gmc.go
@@ -1,17 +1,12 @@
 package gmc
 
 import (
-	"github.com/snail007/gmc/core"
+	gmcapp "github.com/snail007/gmc/app"
+	gmcconfig "github.com/snail007/gmc/config"
+	gmccore "github.com/snail007/gmc/core"
+	gmcdb "github.com/snail007/gmc/db"
 	gmcmysql "github.com/snail007/gmc/db/mysql"
 	gmcsqlite3 "github.com/snail007/gmc/db/sqlite3"
-	gmci18n "github.com/snail007/gmc/i18n"
-	gmchttputil "github.com/snail007/gmc/util/http"
-	"net/http"
-
-	gmcconfig "github.com/snail007/gmc/config"
-
-	gmcapp "github.com/snail007/gmc/app"
-	gmcdb "github.com/snail007/gmc/db"
 	gmcerr "github.com/snail007/gmc/error"
 	gmccontroller "github.com/snail007/gmc/http/controller"
 	gmccookie "github.com/snail007/gmc/http/cookie"
@@ -19,55 +14,87 @@ import (
 	gmchttpserver "github.com/snail007/gmc/http/server"
 	gmcsession "github.com/snail007/gmc/http/session"
 	gmctemplate "github.com/snail007/gmc/http/template"
+	gmci18n "github.com/snail007/gmc/i18n"
+	gmchttputil "github.com/snail007/gmc/util/http"
 	gmcmap "github.com/snail007/gmc/util/map"
+	"net/http"
 )
 
 type (
-	// app
+	// Alias of type gmcapp.GMCApp
 	APP         = gmcapp.GMCApp
+	// Alias of type gmcapp.ServiceItem
 	ServiceItem = gmcapp.ServiceItem
+	// Alias of type gmccore.Service
 	Service     = gmccore.Service
+	// Alias of type gmcconfig.Config
 	Config      = gmcconfig.Config
-
-	// database
+	// Alias of type gmcdb.ResultSet
 	ResultSet = gmcdb.ResultSet
+	// Alias of type gmcdb.Cache
 	DBCache   = gmcdb.Cache
+	// Alias of type gmcmysql.DB
 	MySQL     = gmcmysql.DB
+	// Alias of type gmcsqlite3.DB
 	SQLite3   = gmcsqlite3.DB
-
-	// http server
+	// Alias of type gmccontroller.Controller
 	Controller    = gmccontroller.Controller
+	// Alias of type gmccookie.Cookies
 	Cookie        = gmccookie.Cookies
+	// Alias of type gmccookie.Options
 	CookieOptions = gmccookie.Options
+	// Alias of type gmcrouter.HTTPRouter
 	Router        = gmcrouter.HTTPRouter
+	// Alias of type gmchttpserver.HTTPServer
 	HTTPServer    = gmchttpserver.HTTPServer
+	// Alias of type gmchttpserver.APIServer
 	APIServer     = gmchttpserver.APIServer
+	// Alias of type gmcsession.Session
 	Session       = gmcsession.Session
+	// Alias of type gmcsession.Store
 	SessionStore  = gmcsession.Store
+	// Alias of type gmctemplate.Template
 	Template      = gmctemplate.Template
+	// Alias of type http.ResponseWriter
 	W             = http.ResponseWriter
+	// Alias of type *http.Request
 	R             = *http.Request
+	// Alias of type *gmcrouter.Ctx
 	C             = *gmcrouter.Ctx
-
-	// data
+	// Alias of type gmcmap.Map
 	Map = gmcmap.Map
-
-	// Map
+	// Alias of type map[string]interface{}
 	M   = map[string]interface{}
+	// Alias of type map[interface{}]interface{}
 	Mii = map[interface{}]interface{}
+	// Alias of type map[string]string
 	Mss = map[string]string
-
-	// error
+	// Alias of type gmcerr.Error
 	Error = gmcerr.Error
 )
 
 var (
-	// Errors
+
+	// Shortcut of gmcerr.Stack.
+	// Acquires the full stack, returns string.
 	StackE = gmcerr.Stack
+
+	// Shortcut of gmcerr.Errorf
+	// Create a gmcerr.Error with format string.
 	Errorf = gmcerr.Errorf
+
+	// Shortcut of gmcerr.Wrap
+	// Wrap an error or string to a gmc.Error.
 	WrapE  = gmcerr.Wrap
+
+	// Shortcut of gmchttputil.StopE
+	// StopE will exit controller method if error is not nil.
+	// First argument is an error.
+	// Secondary argument is fail function, it be called if error is not nil.
+	// Third argument is success function, it be called if error is nil.
 	StopE  = gmchttputil.StopE
 
-	// i18n
+	// Shortcut of gmci18n.Tr
+	// This only worked after gmci18n.Init called.
 	Tr = gmci18n.Tr
 )
--- helper.go
+++ helper.go
@@ -26,106 +26,154 @@ var (
 	I18n = &I18n0{}
 )
 
-// New stuff helper
+// #########################################
+// # New stuff helper
+// #########################################
 type New0 struct {
 }
 
+// Config creates a new object of gmc.Config
 func (s *New0) Config() *gmcconfig.Config {
 	return gmcconfig.New()
 }
-func (s *New0) ConfigFile(file string) (cfg *gmcconfig.Config,err error) {
+
+// ConfigFile creates a new object of gmc.Config from a toml file.
+func (s *New0) ConfigFile(file string) (cfg *gmcconfig.Config, err error) {
 	return gmcconfig.NewFile(file)
 }
+
+// App creates an new object of gmc.APP
 func (s *New0) App() *APP {
 	return gmcapp.New()
 }
 
+// Tr creates an new object of gmci18n.I18nTool
+// This only worked after gmc.I18n.Init() called.
+// lang is the language translation to.
 func (s *New0) Tr(lang string) *gmci18n.I18nTool {
-	tool:=gmci18n.NewI18nTool(gmci18n.Tr)
+	tool := gmci18n.NewI18nTool(gmci18n.Tr)
 	tool.Lang(lang)
 	return tool
 }
 
+// AppDefault creates a new object of APP and search config file locations:
+// ./app.toml or ./conf/app.toml or ./config/app.toml
 func (s *New0) AppDefault() *APP {
 	return gmcapp.Default()
 }
 
+// Router creates a new object of gmc.Router
 func (s *New0) Router() *gmcrouter.HTTPRouter {
 	return gmcrouter.NewHTTPRouter()
 }
 
+// HTTPServer creates a new object of gmc.HTTPServer
 func (s *New0) HTTPServer() *gmchttpserver.HTTPServer {
 	return gmchttpserver.New()
 }
 
+// APIServer creates a new object of gmc.APIServer
 func (s *New0) APIServer(address string) *gmchttpserver.APIServer {
 	return gmchttpserver.NewAPIServer(address)
 }
 
-func (s *New0) APIServerDefault(cfg *gmcconfig.Config) (api *gmchttpserver.APIServer,err error) {
+// APIServer creates a new object of gmc.APIServer and initialized from app.toml [apiserver] section.
+// cfg is a gmc.Config object contains section [apiserver] in `app.toml`.
+func (s *New0) APIServerDefault(cfg *gmcconfig.Config) (api *gmchttpserver.APIServer, err error) {
 	return gmchttpserver.NewDefaultAPIServer(cfg)
 }
 
+// Map creates a gmc.Map object, gmc.Map's keys are sequenced.
 func (s *New0) Map() *_map.Map {
 	return _map.NewMap()
 }
 
+// Error creates a gmc.Error from an error or string, the gmc.Error
+// keeps the full stack information.
 func (s *New0) Error(e interface{}) error {
 	return gmcerr.New(e)
 }
 
-// Database helper
+// ##################################################
+// # Database helper
+// # Init Must be called firstly with config object
+// # of app.toml.
+// ##################################################
 type DB0 struct {
 }
 
+// Init initialize the db group objects from a config object
+// contains app.toml section [database].
 func (s *DB0) Init(cfg *gmcconfig.Config) error {
 	return gmcdbhelper.Init(cfg)
 }
 
+// DB acquires the default db group object, you must be call Init firstly.
+// And you must assert it to the correct type to use.
 func (s *DB0) DB(id ...string) interface{} {
 	return gmcdbhelper.DB(id...)
 }
 
+// MySQL acquires the mysql db group object, you must be call Init firstly.
 func (s *DB0) MySQL(id ...string) *gmcmysql.DB {
 	return gmcdbhelper.DBMySQL(id...)
 }
 
+// SQLite3 acquires the sqlite3 db group object, you must be call Init firstly.
 func (s *DB0) SQLite3(id ...string) *gmcsqlite3.DB {
 	return gmcdbhelper.DBSQLite3(id...)
 }
 
-// Cache helper
+// #################################################
+// # Cache helper
+// # Init Must be called firstly with config object
+// # of app.toml.
+// ##################################################
 type Cache0 struct {
 }
 
+// Init initialize the cache group objects from a config object
+// contains app.toml section [cache].
 func (s *Cache0) Init(cfg *gmcconfig.Config) error {
 	return gmccachehelper.Init(cfg)
 }
 
+// Cache acquires the default cache object, you must be call Init firstly.
 func (s *Cache0) Cache(id ...string) gmccore.Cache {
 	return gmccachehelper.Cache(id...)
 }
 
+// Redis acquires the default redis cache object, you must be call Init firstly.
 func (s *Cache0) Redis(id ...string) gmccore.Cache {
 	return gmccachehelper.Redis(id...)
 }
 
+// File acquires the default file cache object, you must be call Init firstly.
 func (s *Cache0) File(id ...string) gmccore.Cache {
 	return gmccachehelper.File(id...)
 }
 
+// Memory acquires the default memory cache object, you must be call Init firstly.
 func (s *Cache0) Memory(id ...string) gmccore.Cache {
 	return gmccachehelper.Memory(id...)
 }
 
-// I18n helper
+// ##################################################
+// # I18n helper
+// # Init Must be called firstly with config object
+// # of app.toml.
+// ##################################################
 type I18n0 struct {
 }
 
+// Init initialize the i18n object from a config object
+// contains app.toml section [i18n].
 func (s *I18n0) Init(cfg *gmcconfig.Config) error {
 	return gmci18n.Init(cfg)
 }
 
-func (s *I18n0) Tr(lang, key string, defaultMessage ...string) string{
-	return gmci18n.Tr(lang,key,defaultMessage...)
+// Tr search the key in the `lang` i18n file, if not found, then search the
+// `fallback` (default) lang file, if both fail `defaultMessage` will be returned. You must be call Init firstly.
+func (s *I18n0) Tr(lang, key string, defaultMessage ...string) string {
+	return gmci18n.Tr(lang, key, defaultMessage...)
 }

e40d6a3e0cbb213bdee58f88138094e5fb729e5a
arraykeys <arraykeys@gmail.com>
2021-01-28 14:29:28 +0800

    add testing case for gsync

---
+++ util/sync/once_test.go
@@ -0,0 +1,22 @@
+package gsync
+
+import (
+	assert2 "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLoadOnce(t *testing.T) {
+	assert := assert2.New(t)
+	once := LoadOnce("test")
+	assert.NotNil(once)
+	RemoveOnce("test")
+	once1 := LoadOnce("test")
+	assert.NotNil(once, once1)
+}
+
+func TestLoadOnce_1(t *testing.T) {
+	assert := assert2.New(t)
+	once := LoadOnce("test")
+	once1 := LoadOnce("test")
+	assert.Equal(once, once1)
+}

7b5fe08584d2f3ed2e6a1c78ad3d1e3f75628531
arraykeys <arraykeys@gmail.com>
2021-02-07 10:22:53 +0800

    github actions

--- .ci/tests.db.sql
+++ .ci/tests.db.sql
@@ -5,7 +5,7 @@ SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
+DROP DATABASE `test`;
 CREATE DATABASE `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `test`;
 

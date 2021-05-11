2f26631367444e270d4da39a26e37f55708c42a7
snail007 <arraykeys@gmail.com>
2021-05-11 22:50:10 +0800

    gpool

--- util/gpool/gpool_test.go
+++ util/gpool/gpool_test.go
@@ -155,16 +155,14 @@ func TestGPool_MaxWaitCount(t *testing.T) {
 	p.SetDebug(true)
 	assert.True(p.IsDebug())
 	p.SetMaxTaskAwaitCount(2)
-
-	p.Submit(func() {
-		time.Sleep(time.Second)
-	})
-
 	p.ResetTo(2)
 	assert.Equal(2,p.WorkerCount())
 	p.ResetTo(1)
 	assert.Equal(1,p.WorkerCount())
 
+	p.Submit(func() {
+		time.Sleep(time.Second)
+	})
 	p.Submit(func() {
 		time.Sleep(time.Second)
 	})

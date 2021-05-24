e15e49a1159dcf5adf6c55c90342ca864278fc60
arraykeys <arraykeys@gmail.com>
2021-05-24 17:45:08 +0800

    size_testing

--- util/byte/size.go
+++ util/byte/size.go
@@ -25,7 +25,6 @@ const (
 
 	fnUnmarshalText string = "UnmarshalText"
 	maxUint64       uint64 = (1 << 64) - 1
-	cutoff                 = maxUint64 / 10
 )
 
 var ErrBits = errors.New("unit with capital unit prefix and lower case unit (b) - bits, not bytes ")
@@ -53,59 +52,91 @@ func (b ByteSize) Bytes() uint64 {
 }
 
 func (b ByteSize) KBytes() float64 {
+	if b < KB {
+		return 0
+	}
 	v := b / KB
 	r := b % KB
 	return float64(v) + float64(r)/float64(KB)
 }
 
 func (b ByteSize) MBytes() float64 {
+	if b < MB {
+		return 0
+	}
 	v := b / MB
 	r := b % MB
 	return float64(v) + float64(r)/float64(MB)
 }
 
 func (b ByteSize) GBytes() float64 {
+	if b < GB {
+		return 0
+	}
 	v := b / GB
 	r := b % GB
 	return float64(v) + float64(r)/float64(GB)
 }
 
 func (b ByteSize) TBytes() float64 {
+	if b < TB {
+		return 0
+	}
 	v := b / TB
 	r := b % TB
 	return float64(v) + float64(r)/float64(TB)
 }
 
 func (b ByteSize) PBytes() float64 {
+	if b < PB {
+		return 0
+	}
 	v := b / PB
 	r := b % PB
 	return float64(v) + float64(r)/float64(PB)
 }
 
 func (b ByteSize) EBytes() float64 {
+	if b < EB {
+		return 0
+	}
 	v := b / EB
 	r := b % EB
 	return float64(v) + float64(r)/float64(EB)
 }
 
-func (b ByteSize) String() string {
+func (b ByteSize) HumanReadable() (s string) {
+	uint := "B"
+	defer func() {
+		if strings.Contains(s, ".") {
+			s = strings.TrimRight(s, "0")
+			s = strings.TrimSuffix(s, ".")
+		}
+		s += " " + uint
+	}()
 	switch {
 	case b == 0:
-		return fmt.Sprint("0B")
-	case b%EB == 0:
-		return fmt.Sprintf("%dEB", b/EB)
-	case b%PB == 0:
-		return fmt.Sprintf("%dPB", b/PB)
-	case b%TB == 0:
-		return fmt.Sprintf("%dTB", b/TB)
-	case b%GB == 0:
-		return fmt.Sprintf("%dGB", b/GB)
-	case b%MB == 0:
-		return fmt.Sprintf("%dMB", b/MB)
-	case b%KB == 0:
-		return fmt.Sprintf("%dKB", b/KB)
+		return fmt.Sprint("0")
+	case b/EB >= 1:
+		uint = "EB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(EB))
+	case b/PB >= 1:
+		uint = "PB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(PB))
+	case b/TB >= 1:
+		uint = "TB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(TB))
+	case b/GB >= 1:
+		uint = "GB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(GB))
+	case b/MB >= 1:
+		uint = "MB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(MB))
+	case b/KB >= 1:
+		uint = "KB"
+		return fmt.Sprintf("%.2f", float64(b)/float64(KB))
 	default:
-		return fmt.Sprintf("%dB", b)
+		return fmt.Sprintf("%d", b)
 	}
 }
 
@@ -113,67 +144,33 @@ func (b ByteSize) HR() string {
 	return b.HumanReadable()
 }
 
-func (b ByteSize) HumanReadable() string {
-	switch {
-	case b > EB:
-		return fmt.Sprintf("%.1f EB", b.EBytes())
-	case b > PB:
-		return fmt.Sprintf("%.1f PB", b.PBytes())
-	case b > TB:
-		return fmt.Sprintf("%.1f TB", b.TBytes())
-	case b > GB:
-		return fmt.Sprintf("%.1f GB", b.GBytes())
-	case b > MB:
-		return fmt.Sprintf("%.1f MB", b.MBytes())
-	case b > KB:
-		return fmt.Sprintf("%.1f KB", b.KBytes())
-	default:
-		return fmt.Sprintf("%d B", b)
-	}
+func (b ByteSize) String() (s string) {
+	return strings.Replace(b.HumanReadable(), " ", "", 1)
 }
 
 func (b ByteSize) MarshalText() ([]byte, error) {
 	return []byte(b.String()), nil
 }
 
-func (b *ByteSize) UnmarshalText(t []byte) error {
-	var val uint64
+func (b *ByteSize) UnmarshalText(t []byte) (err error) {
+	var val float64 = -1
 	var unit string
-
+	if len(t) == 0 {
+		return &strconv.NumError{Func: fnUnmarshalText, Err: ErrBits}
+	}
 	// copy for error message
 	t0 := t
-
-	var c byte
-	var i int
-
-ParseLoop:
-	for i < len(t) {
-		c = t[i]
-		switch {
-		case '0' <= c && c <= '9':
-			if val > cutoff {
-				goto Overflow
+	for i, v := range t {
+		if !(v >= '0' && v <= '9' || v == '.') {
+			unit = strings.TrimSpace(string(t[i:]))
+			val, err = strconv.ParseFloat(string(t[:i]), 10)
+			if err != nil {
+				return
 			}
-
-			c = c - '0'
-			val *= 10
-
-			if val > val+uint64(c) {
-				// val+v overflows
-				goto Overflow
-			}
-			val += uint64(c)
-			i++
-
-		default:
-			if i == 0 {
-				goto SyntaxError
-			}
-			break ParseLoop
+			break
 		}
 	}
 
-	unit = strings.TrimSpace(string(t[i:]))
 	switch unit {
 	case "Kb", "Mb", "Gb", "Tb", "Pb", "Eb":
 		goto BitsError
@@ -181,60 +178,66 @@ ParseLoop:
 	unit = strings.ToLower(unit)
 	switch unit {
 	case "", "b", "byte":
-		// do nothing - already in bytes
-
+		defer func() {
+			*b = ByteSize(val)
+		}()
+		if val >= 0 {
+			return
+		}
+		val, err = strconv.ParseFloat(string(t), 10)
+		if err != nil {
+			return
+		}
 	case "k", "kb", "kilo", "kilobyte", "kilobytes":
-		if val > maxUint64/uint64(KB) {
+		if val > float64(maxUint64/uint64(KB)) {
 			goto Overflow
 		}
-		val *= uint64(KB)
+		val *= float64(KB)
 
 	case "m", "mb", "mega", "megabyte", "megabytes":
-		if val > maxUint64/uint64(MB) {
+		if val > float64(maxUint64/uint64(MB)) {
 			goto Overflow
 		}
-		val *= uint64(MB)
+		val *= float64(MB)
 
 	case "g", "gb", "giga", "gigabyte", "gigabytes":
-		if val > maxUint64/uint64(GB) {
+		if val > float64(maxUint64/uint64(GB)) {
 			goto Overflow
 		}
-		val *= uint64(GB)
+		val *= float64(GB)
 
 	case "t", "tb", "tera", "terabyte", "terabytes":
-		if val > maxUint64/uint64(TB) {
+		if val > float64(maxUint64/uint64(TB)) {
 			goto Overflow
 		}
-		val *= uint64(TB)
+		val *= float64(TB)
 
 	case "p", "pb", "peta", "petabyte", "petabytes":
-		if val > maxUint64/uint64(PB) {
+		if val > float64(maxUint64/uint64(PB)) {
 			goto Overflow
 		}
-		val *= uint64(PB)
-
-	case "E", "EB", "e", "eb", "eB":
-		if val > maxUint64/uint64(EB) {
+		val *= float64(PB)
+	case "e", "eb":
+		if val > float64(maxUint64/uint64(EB)) {
 			goto Overflow
 		}
-		val *= uint64(EB)
+		val *= float64(EB)
 
 	default:
 		goto SyntaxError
 	}
-
 	*b = ByteSize(val)
 	return nil
 
 Overflow:
 	*b = ByteSize(maxUint64)
-	return &strconv.NumError{fnUnmarshalText, string(t0), strconv.ErrRange}
+	return &strconv.NumError{Func: fnUnmarshalText, Num: string(t0), Err: strconv.ErrRange}
 
 SyntaxError:
 	*b = 0
-	return &strconv.NumError{fnUnmarshalText, string(t0), strconv.ErrSyntax}
+	return &strconv.NumError{Func: fnUnmarshalText, Num: string(t0), Err: strconv.ErrSyntax}
 
 BitsError:
 	*b = 0
-	return &strconv.NumError{fnUnmarshalText, string(t0), ErrBits}
+	return &strconv.NumError{Func: fnUnmarshalText, Num: string(t0), Err: ErrBits}
 }
---
+++ util/byte/size_test.go
@@ -0,0 +1,364 @@
+package gbyte
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestByteSize_Bytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want uint64
+	}{
+		{"B", 1 * KB, uint64(1024 * B)},
+		{"MB", 1 * MB, uint64(1024 * KB)},
+		{"GB", 1 * GB, uint64(1024 * MB)},
+		{"TB", 1 * TB, uint64(1024 * GB)},
+		{"PB", 1 * PB, uint64(1024 * TB)},
+		{"EB", 1 * EB, uint64(1024 * PB)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.Bytes(); got != tt.want {
+				t.Errorf("Bytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_EBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", 1024 * 1024 * 1024 * 1024 * 1024 * KB, 1},
+		{"MB", 1024 * 1024 * 1024 * 1024 * MB, 1},
+		{"GB", 1024 * 1024 * 1024 * GB, 1},
+		{"TB", 1024 * 1024 * TB, 1},
+		{"PB", 1024 * PB, 1},
+		{"EB", 1 * EB, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.EBytes(); got != tt.want {
+				t.Errorf("EBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_GBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", 1024 * 1024 * KB, 1},
+		{"MB", 1024 * MB, 1},
+		{"GB", GB, 1},
+		{"TB", TB, float64(1024)},
+		{"PB", PB, float64(1024 * 1024)},
+		{"EB", EB, float64(1024 * 1024 * 1024)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.GBytes(); got != tt.want {
+				t.Errorf("GBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_HumanReadable(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want string
+	}{
+		{"B", 0, "0 B"},
+		{"B", 2000 * B, "1.95 KB"},
+		{"B", 2048 * B, "2 KB"},
+		{"B", 2100 * B, "2.05 KB"},
+		{"KB", 2000 * KB, "1.95 MB"},
+		{"KB", 2048 * KB, "2 MB"},
+		{"KB", 2100 * KB, "2.05 MB"},
+		{"MB", 2000 * MB, "1.95 GB"},
+		{"MB", 2048 * MB, "2 GB"},
+		{"MB", 2100 * MB, "2.05 GB"},
+		{"GB", 2000 * GB, "1.95 TB"},
+		{"GB", 2048 * GB, "2 TB"},
+		{"GB", 2100 * GB, "2.05 TB"},
+		{"TB", 2000 * TB, "1.95 PB"},
+		{"TB", 2048 * TB, "2 PB"},
+		{"TB", 2100 * TB, "2.05 PB"},
+		{"PB", 2000 * PB, "1.95 EB"},
+		{"PB", 2048 * PB, "2 EB"},
+		{"PB", 2100 * PB, "2.05 EB"},
+		{"EB", 2 * EB, "2 EB"},
+		{"EB", 10 * EB, "10 EB"},
+		{"EB", 15 * EB, "15 EB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.HR(); got != tt.want {
+				t.Errorf("HumanReadable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_KBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", B, 0},
+		{"KB", KB, 1},
+		{"MB", MB, 1024},
+		{"GB", GB, 1024 * 1024},
+		{"TB", TB, 1024 * 1024 * 1024},
+		{"PB", PB, 1024 * 1024 * 1024 * 1024},
+		{"EB", EB, 1024 * 1024 * 1024 * 1024 * 1024},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.KBytes(); got != tt.want {
+				t.Errorf("KBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_MBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", B, 0},
+		{"KB", KB, 0},
+		{"MB", MB, 1},
+		{"GB", GB, 1024},
+		{"TB", TB, 1024 * 1024},
+		{"PB", PB, 1024 * 1024 * 1024},
+		{"EB", EB, 1024 * 1024 * 1024 * 1024},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.MBytes(); got != tt.want {
+				t.Errorf("MBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_MarshalText(t *testing.T) {
+	tests := []struct {
+		name    string
+		b       ByteSize
+		want    []byte
+		wantErr bool
+	}{
+		{"B", B, []byte("1B"), false},
+		{"KB", KB, []byte("1KB"), false},
+		{"MB", MB, []byte("1MB"), false},
+		{"GB", GB, []byte("1GB"), false},
+		{"TB", TB, []byte("1TB"), false},
+		{"PB", PB, []byte("1PB"), false},
+		{"EB", EB, []byte("1EB"), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.b.MarshalText()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MarshalText() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MarshalText() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_PBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", B, 0},
+		{"KB", KB, 0},
+		{"MB", MB, 0},
+		{"GB", GB, 0},
+		{"TB", TB, 0},
+		{"PB", PB, 1},
+		{"EB", EB, 1024},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.PBytes(); got != tt.want {
+				t.Errorf("PBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_String(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want string
+	}{
+		{"B", B, "1B"},
+		{"KB", KB, "1KB"},
+		{"MB", MB, "1MB"},
+		{"GB", GB, "1GB"},
+		{"TB", TB, "1TB"},
+		{"PB", PB, "1PB"},
+		{"EB", EB, "1EB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_TBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		b    ByteSize
+		want float64
+	}{
+		{"B", B, 0},
+		{"KB", KB, 0},
+		{"MB", MB, 0},
+		{"GB", GB, 0},
+		{"TB", TB, 1},
+		{"PB", PB, 1024},
+		{"EB", EB, 1024 * 1024},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.TBytes(); got != tt.want {
+				t.Errorf("TBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByteSize_UnmarshalText(t *testing.T) {
+	type args struct {
+		t []byte
+	}
+	tests := []struct {
+		name    string
+		b       ByteSize
+		args    args
+		wantErr bool
+	}{
+		{"B", 1, args{t: []byte("1.1B")}, false},
+		{"B", B, args{t: []byte("1B")}, false},
+		{"KB", KB, args{[]byte("1KB")}, false},
+		{"MB", MB, args{[]byte("1MB")}, false},
+		{"GB", GB, args{[]byte("1GB")}, false},
+		{"TB", TB, args{[]byte("1TB")}, false},
+		{"PB", PB, args{[]byte("1PB")}, false},
+		{"EB", EB, args{[]byte("1EB")}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			old := tt.b
+			if err := tt.b.UnmarshalText(tt.args.t); (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshalText() error = %v, wantErr %v", err, tt.wantErr)
+			} else if tt.b != old {
+				t.Errorf("UnmarshalText() want = %v, actual %v", tt.b, old)
+			}
+		})
+	}
+}
+
+func TestSizeToStr(t *testing.T) {
+	type args struct {
+		bytes uint64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantS   string
+		wantErr bool
+	}{
+ 		{"B", args{1}, "1 B", false},
+		{"KB", args{uint64(1.5 * float64(KB))}, "1.5 KB", false},
+		{"KB", args{uint64(2 * float64(KB))}, "2 KB", false},
+		{"MB", args{uint64(1.5 * float64(MB))}, "1.5 MB", false},
+		{"MB", args{uint64(2 * float64(MB))}, "2 MB", false},
+		{"GB", args{uint64(1.5 * float64(GB))}, "1.5 GB", false},
+		{"GB", args{uint64(2 * float64(GB))}, "2 GB", false},
+		{"TB", args{uint64(1.5 * float64(TB))}, "1.5 TB", false},
+		{"TB", args{uint64(2 * float64(TB))}, "2 TB", false},
+		{"PB", args{uint64(1.5 * float64(PB))}, "1.5 PB", false},
+		{"PB", args{uint64(2 * float64(PB))}, "2 PB", false},
+		{"EB", args{uint64(1.5 * float64(EB))}, "1.5 EB", false},
+		{"EB", args{uint64(2 * float64(EB))}, "2 EB", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotS, err := SizeToStr(tt.args.bytes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SizeToStr() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotS != tt.wantS {
+				t.Errorf("SizeToStr() gotS = %v, want %v", gotS, tt.wantS)
+			}
+		})
+	}
+}
+
+func TestStrToSize(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantBytes uint64
+		wantErr   bool
+	}{
+		{"Kb", args{""}, 0, true},
+		{"B", args{""}, 0, true},
+		{"B", args{"1.x"}, 0, true},
+		{"B", args{"1 B"}, 1, false},
+		{"KB", args{"1.5 KB"}, uint64(1.5 * float64(KB)), false},
+		{"KB", args{"2 KB"}, uint64(2 * float64(KB)), false},
+		{"MB", args{"1.5 MB"}, uint64(1.5 * float64(MB)), false},
+		{"MB", args{"2 MB"}, uint64(2 * float64(MB)), false},
+		{"GB", args{"1.5 GB"}, uint64(1.5 * float64(GB)), false},
+		{"GB", args{"2 GB"}, uint64(2 * float64(GB)), false},
+		{"TB", args{"1.5 TB"}, uint64(1.5 * float64(TB)), false},
+		{"TB", args{"2 TB"}, uint64(2 * float64(TB)), false},
+		{"PB", args{"1.5 PB"}, uint64(1.5 * float64(PB)), false},
+		{"PB", args{"2 PB"}, uint64(2 * float64(PB)), false},
+		{"EB", args{"1.5 EB"}, uint64(1.5 * float64(EB)), false},
+		{"EB", args{"2 EB"}, uint64(2 * float64(EB)), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotBytes, err := StrToSize(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StrToSize() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotBytes != tt.wantBytes {
+				t.Errorf("StrToSize() gotBytes = %v, want %v", gotBytes, tt.wantBytes)
+			}
+		})
+	}
+}

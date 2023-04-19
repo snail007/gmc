da3a39809b1ce667805ebf1ae328d8e39ba9929e
arraykeys <arraykeys@gmail.com>
2023-04-19 12:27:18 +0800

    ghttp url

---
+++ util/http/url.go
@@ -0,0 +1,51 @@
+package ghttp
+
+import "net/url"
+
+type URLBuilder struct {
+	*url.URL
+	query map[string]string
+}
+
+func NewURLBuilder() *URLBuilder {
+	return &URLBuilder{
+		URL:   &url.URL{},
+		query: map[string]string{},
+	}
+}
+
+func (s *URLBuilder) HTTP() *URLBuilder {
+	s.URL.Scheme = "http"
+	return s
+}
+
+func (s *URLBuilder) String() string {
+	return AppendQuery(s.URL.String(), s.query)
+}
+
+func (s *URLBuilder) HTTPS() *URLBuilder {
+	s.URL.Scheme = "https"
+	return s
+}
+
+func (s *URLBuilder) Scheme(scheme string) *URLBuilder {
+	s.URL.Scheme = scheme
+	return s
+}
+
+func (s *URLBuilder) Host(host string) *URLBuilder {
+	s.URL.Host = host
+	return s
+}
+
+func (s *URLBuilder) Path(path string) *URLBuilder {
+	s.URL.Path = path
+	return s
+}
+
+func (s *URLBuilder) Query(data map[string]string) *URLBuilder {
+	for k, v := range data {
+		s.query[k] = v
+	}
+	return s
+}
---
+++ util/http/url_test.go
@@ -0,0 +1,16 @@
+package ghttp
+
+import (
+	gmap "github.com/snail007/gmc/util/map"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestURLBuilder_Query(t *testing.T) {
+	link := NewURLBuilder().HTTP().Host("www.example.com").Path("/abc").Query(gmap.Mss{"a": "b", "c": "d"})
+	assert.Equal(t, "http://www.example.com/abc?a=b&c=d", link.String())
+	link = NewURLBuilder().HTTPS().Host("www.example.com").Path("/abc").Query(gmap.Mss{"a": "b", "c": "d"})
+	assert.Equal(t, "https://www.example.com/abc?a=b&c=d", link.String())
+	link = NewURLBuilder().Scheme("ftp").Host("www.example.com").Path("/abc").Query(gmap.Mss{"a": "b", "c": "d"})
+	assert.Equal(t, "ftp://www.example.com/abc?a=b&c=d", link.String())
+}

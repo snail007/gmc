fb02632f40388db0409b1580d472051b8bd55f79
arraykeys <arraykeys@gmail.com>
2023-05-16 11:37:33 +0800

    glog

--- module/middleware/accesslog/accesslog.go
+++ module/middleware/accesslog/accesslog.go
@@ -24,7 +24,13 @@ func newFromConfig(c gcore.Config) *accesslog {
 	cfg := c.Sub("accesslog")
 	logger := glog.New().(*glog.Logger)
 	logger.SetFlag(gcore.LogFlagShort)
-	logger.SetOutput(glog.NewFileWriter(cfg.GetString("filename"), cfg.GetString("dir"), cfg.GetString("archive_dir"), cfg.GetBool("gzip")))
+	logger.SetOutput(glog.NewFileWriter(&glog.FileWriterOption{
+		Filename:      cfg.GetString("filename"),
+		LogsDir:       cfg.GetString("dir"),
+		ArchiveDir:    cfg.GetString("archive_dir"),
+		IsGzip:        cfg.GetBool("gzip"),
+		AliasFilename: cfg.GetString("filename_alias"),
+	}))
 	logger.EnableAsync()
 	return &accesslog{
 		format: cfg.GetString("format"),

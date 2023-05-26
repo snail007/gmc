086fd10d16ccf0d8f8262d4d813a0c23076e400f
snail007 <arraykeys@gmail.com>
2023-05-26 19:20:19 +0800

    gatomic NewIntxxx NewUIntxxx NewFloatxxx

--- util/sync/atomic/atomic.go
+++ util/sync/atomic/atomic.go
@@ -31,12 +31,8 @@ type Any struct {
 	val interface{}
 }
 
-func NewAny(x ...interface{}) *Any {
-	var val interface{}
-	if len(x) == 1 && x[0] != nil {
-		val = x[0]
-	}
-	return &Any{val: val}
+func NewAny(initValue interface{}) *Any {
+	return &Any{val: initValue}
 }
 
 func (s *Any) SetVal(x interface{}) {
@@ -54,7 +50,7 @@ func (s *Any) Val() (x interface{}) {
 
 func (s *Any) GetAndSet(f func(oldVal interface{}) (newVal interface{})) {
 	s.set(func() {
-		s.SetVal(f(s.val))
+		s.val = f(s.val)
 	})
 }
 
@@ -63,11 +59,9 @@ type Value struct {
 	val atomic.Value
 }
 
-func NewValue(x ...interface{}) *Value {
+func NewValue(initValue interface{}) *Value {
 	var val atomic.Value
-	if len(x) == 1 && x[0] != nil {
-		val.Store(x[0])
-	}
+	val.Store(initValue)
 	return &Value{val: val}
 }
 
@@ -95,12 +89,8 @@ type String struct {
 	val string
 }
 
-func NewString(val ...string) *String {
-	v := ""
-	if len(val) == 1 {
-		v = val[0]
-	}
-	return &String{val: v}
+func NewString(initValue string) *String {
+	return &String{val: initValue}
 }
 
 func (s *String) IsEmpty() bool {
@@ -128,8 +118,12 @@ type Bool struct {
 	val *int32
 }
 
-func NewBool() *Bool {
+func NewBool(initValue bool) *Bool {
 	a := new(int32)
+	*a = 0
+	if initValue {
+		*a = 1
+	}
 	return &Bool{val: a}
 }
 
@@ -154,10 +148,11 @@ type Bytes struct {
 	bytes []byte
 }
 
-func NewBytes(val ...[]byte) *Bytes {
+func NewBytes(initValue []byte) *Bytes {
 	var v []byte
-	if len(val) == 1 {
-		v = val[0]
+	if len(initValue) > 0 {
+		v = make([]byte, len(initValue))
+		copy(v, initValue)
 	}
 	return &Bytes{bytes: v}
 }
@@ -169,13 +164,22 @@ func (s *Bytes) Bytes() (x []byte) {
 	return
 }
 
+func (s *Bytes) CloneBytes() (x []byte) {
+	s.get(func() {
+		x = make([]byte, len(s.bytes))
+		copy(x, s.bytes)
+	})
+	return
+}
+
 func (s *Bytes) Len() int {
 	return len(s.Bytes())
 }
 
 func (s *Bytes) SetBytes(data []byte) {
 	s.set(func() {
-		s.bytes = data
+		s.bytes = make([]byte, len(data))
+		copy(s.bytes, data)
 	})
 }
 
@@ -185,35 +189,442 @@ func (s *Bytes) Append(bytes []byte) {
 	})
 }
 
+type Int8 struct {
+	baseValue
+	val int8
+}
+
+func NewInt8(initValue int8) *Int8 {
+	return &Int8{val: initValue}
+}
+
+func (s *Int8) Increase(cnt int8) (newValue int8) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int8) Decrease(cnt int8) (newValue int8) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int8) SetVal(cnt int8) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Int8) Val() (value int8) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Int8) GetAndSet(f func(oldVal int8) (newVal int8)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
 type Int struct {
 	baseValue
-	val *int32
+	val int
 }
 
-func NewInt(defaultValue int32) *Int {
-	a := new(int32)
-	*a = defaultValue
-	return &Int{val: a}
+func NewInt(initValue int) *Int {
+	return &Int{val: initValue}
 }
 
-func (s *Int) Increase(cnt int32) int32 {
-	return atomic.AddInt32(s.val, cnt)
-}
-
-func (s *Int) Decrease(cnt int32) int32 {
-	return atomic.AddInt32(s.val, -cnt)
-}
-
-func (s *Int) SetVal(cnt int32) {
-	atomic.StoreInt32(s.val, cnt)
-}
-
-func (s *Int) Val() int32 {
-	return atomic.LoadInt32(s.val)
-}
-
-func (s *Int) GetAndSet(f func(oldVal int32) (newVal int32)) {
+func (s *Int) Increase(cnt int) (newValue int) {
 	s.set(func() {
-		s.SetVal(f(s.Val()))
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int) Decrease(cnt int) (newValue int) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int) SetVal(cnt int) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Int) Val() (value int) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Int) GetAndSet(f func(oldVal int) (newVal int)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Int32 struct {
+	baseValue
+	val int32
+}
+
+func NewInt32(initValue int32) *Int32 {
+	return &Int32{val: initValue}
+}
+
+func (s *Int32) Increase(cnt int32) (newValue int32) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int32) Decrease(cnt int32) (newValue int32) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int32) SetVal(cnt int32) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Int32) Val() (value int32) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Int32) GetAndSet(f func(oldVal int32) (newVal int32)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Int64 struct {
+	baseValue
+	val int64
+}
+
+func NewInt64(initValue int64) *Int64 {
+	return &Int64{val: initValue}
+}
+
+func (s *Int64) Increase(cnt int64) (newValue int64) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int64) Decrease(cnt int64) (newValue int64) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Int64) SetVal(cnt int64) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Int64) Val() (value int64) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Int64) GetAndSet(f func(oldVal int64) (newVal int64)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Uint8 struct {
+	baseValue
+	val uint8
+}
+
+func NewUint8(initValue uint8) *Uint8 {
+	return &Uint8{val: initValue}
+}
+
+func (s *Uint8) Increase(cnt uint8) (newValue uint8) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint8) Decrease(cnt uint8) (newValue uint8) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint8) SetVal(cnt uint8) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Uint8) Val() (value uint8) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Uint8) GetAndSet(f func(oldVal uint8) (newVal uint8)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Uint struct {
+	baseValue
+	val uint
+}
+
+func NewUint(initValue uint) *Uint {
+	return &Uint{val: initValue}
+}
+
+func (s *Uint) Increase(cnt uint) (newValue uint) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint) Decrease(cnt uint) (newValue uint) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint) SetVal(cnt uint) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Uint) Val() (value uint) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Uint) GetAndSet(f func(oldVal uint) (newVal uint)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Uint32 struct {
+	baseValue
+	val uint32
+}
+
+func NewUint32(initValue uint32) *Uint32 {
+	return &Uint32{val: initValue}
+}
+
+func (s *Uint32) Increase(cnt uint32) (newValue uint32) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint32) Decrease(cnt uint32) (newValue uint32) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint32) SetVal(cnt uint32) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Uint32) Val() (value uint32) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Uint32) GetAndSet(f func(oldVal uint32) (newVal uint32)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Uint64 struct {
+	baseValue
+	val uint64
+}
+
+func NewUint64(initValue uint64) *Uint64 {
+	return &Uint64{val: initValue}
+}
+
+func (s *Uint64) Increase(cnt uint64) (newValue uint64) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint64) Decrease(cnt uint64) (newValue uint64) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Uint64) SetVal(cnt uint64) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Uint64) Val() (value uint64) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Uint64) GetAndSet(f func(oldVal uint64) (newVal uint64)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Float32 struct {
+	baseValue
+	val float32
+}
+
+func NewFloat32(initValue float32) *Float32 {
+	return &Float32{val: initValue}
+}
+
+func (s *Float32) Increase(cnt float32) (newValue float32) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Float32) Decrease(cnt float32) (newValue float32) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Float32) SetVal(cnt float32) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Float32) Val() (value float32) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Float32) GetAndSet(f func(oldVal float32) (newVal float32)) {
+	s.set(func() {
+		s.val = f(s.val)
+	})
+}
+
+type Float64 struct {
+	baseValue
+	val float64
+}
+
+func NewFloat64(initValue float64) *Float64 {
+	return &Float64{val: initValue}
+}
+
+func (s *Float64) Increase(cnt float64) (newValue float64) {
+	s.set(func() {
+		s.val += cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Float64) Decrease(cnt float64) (newValue float64) {
+	s.set(func() {
+		s.val -= cnt
+		newValue = s.val
+	})
+	return
+}
+
+func (s *Float64) SetVal(cnt float64) {
+	s.set(func() {
+		s.val = cnt
+	})
+}
+
+func (s *Float64) Val() (value float64) {
+	s.get(func() {
+		value = s.val
+	})
+	return
+}
+
+func (s *Float64) GetAndSet(f func(oldVal float64) (newVal float64)) {
+	s.set(func() {
+		s.val = f(s.val)
 	})
 }
--- util/sync/atomic/atomic_test.go
+++ util/sync/atomic/atomic_test.go
@@ -124,13 +124,13 @@ func TestBool_IsTrue(t *testing.T) {
 }
 
 func TestBool_SetFalse(t *testing.T) {
-	a := NewBool()
+	a := NewBool(true)
 	a.SetFalse()
 	assert2.True(t, a.IsFalse())
 }
 
 func TestBool_SetTrue(t *testing.T) {
-	a := NewBool()
+	a := NewBool(false)
 	a.SetTrue()
 	assert2.True(t, a.IsTrue())
 }
@@ -154,7 +154,7 @@ func TestBytes_SetBytes(t *testing.T) {
 }
 
 func TestString_SetVal(t *testing.T) {
-	a := NewString()
+	a := NewString("")
 	a.SetVal("abc")
 	assert2.Equal(t, "abc", a.Val())
 }
@@ -165,9 +165,17 @@ func TestString_String(t *testing.T) {
 }
 
 func TestBytes_SetBytes1(t *testing.T) {
-	a := NewBytes()
+	a := NewBytes(nil)
 	a.SetBytes([]byte("abc"))
 	assert2.Equal(t, []byte("abc"), a.Bytes())
+}
+
+func TestBytes_CloneBytes(t *testing.T) {
+	a := NewBytes(nil)
+	a.SetBytes([]byte("abc"))
+	b := a.CloneBytes()
+	assert2.NotEqual(t, a, b)
+	assert2.Equal(t, []byte("abc"), b)
 }
 
 func TestAny_SetVal(t *testing.T) {
@@ -182,8 +190,61 @@ func TestAny_SetVal(t *testing.T) {
 	})
 }
 
+func TestNewInt8(t *testing.T) {
+	t.Parallel()
+	cnt := NewInt8(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int8(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int8(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, int8(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal int8) (newVal int8) {
+		assert2.Equal(t, int8(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, int8(100), cnt.Val())
+}
+
 func TestNewInt(t *testing.T) {
+	t.Parallel()
 	cnt := NewInt(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, int(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal int) (newVal int) {
+		assert2.Equal(t, int(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, int(100), cnt.Val())
+}
+
+func TestNewInt32(t *testing.T) {
+	t.Parallel()
+	cnt := NewInt32(0)
 	for i := 0; i < 100; i++ {
 		go func() {
 			cnt.Increase(1)
@@ -205,4 +266,186 @@ func TestNewInt(t *testing.T) {
 		return 100
 	})
 	assert2.Equal(t, int32(100), cnt.Val())
+}
+
+func TestNewInt64(t *testing.T) {
+	t.Parallel()
+	cnt := NewInt64(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int64(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, int64(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, int64(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal int64) (newVal int64) {
+		assert2.Equal(t, int64(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, int64(100), cnt.Val())
+}
+
+func TestNewUint8(t *testing.T) {
+	t.Parallel()
+	cnt := NewUint8(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint8(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint8(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, uint8(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal uint8) (newVal uint8) {
+		assert2.Equal(t, uint8(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, uint8(100), cnt.Val())
+}
+
+func TestNewUint(t *testing.T) {
+	t.Parallel()
+	cnt := NewUint(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, uint(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal uint) (newVal uint) {
+		assert2.Equal(t, uint(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, uint(100), cnt.Val())
+}
+
+func TestNewUint32(t *testing.T) {
+	t.Parallel()
+	cnt := NewUint32(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint32(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint32(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, uint32(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal uint32) (newVal uint32) {
+		assert2.Equal(t, uint32(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, uint32(100), cnt.Val())
+}
+
+func TestNewUint64(t *testing.T) {
+	t.Parallel()
+	cnt := NewUint64(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint64(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, uint64(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, uint64(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal uint64) (newVal uint64) {
+		assert2.Equal(t, uint64(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, uint64(100), cnt.Val())
+}
+
+func TestNewFloat32(t *testing.T) {
+	t.Parallel()
+	cnt := NewFloat32(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, float32(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, float32(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, float32(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal float32) (newVal float32) {
+		assert2.Equal(t, float32(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, float32(100), cnt.Val())
+}
+
+func TestNewFloat64(t *testing.T) {
+	t.Parallel()
+	cnt := NewFloat64(0)
+	for i := 0; i < 100; i++ {
+		go func() {
+			cnt.Increase(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, float64(100), cnt.Val())
+	for i := 0; i < 10; i++ {
+		go func() {
+			cnt.Decrease(1)
+		}()
+	}
+	time.Sleep(time.Second)
+	assert2.Equal(t, float64(90), cnt.Val())
+	cnt.SetVal(10)
+	assert2.Equal(t, float64(10), cnt.Val())
+	cnt.GetAndSet(func(oldVal float64) (newVal float64) {
+		assert2.Equal(t, float64(10), oldVal)
+		return 100
+	})
+	assert2.Equal(t, float64(100), cnt.Val())
 }
